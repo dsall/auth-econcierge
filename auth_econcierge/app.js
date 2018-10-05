3f48b2d11662e08677e87114abb500a654a9d513
dsall0331 <djiby45@outlook.com>
2018-10-05 03:36:08 -0400

    update

--- auth_econcierge/app.js
+++ auth_econcierge/app.js
@@ -8,13 +8,16 @@ var flash = require('connect-flash');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var passport = require('passport');
+
 mongoose.connect(
-    "mongodb+srv://matt:1EQI2P6yWRoMz3qg@cluster0-exl40.mongodb.net/Face-Recog",  { useNewUrlParser: true }
+    "mongodb+srv://matt:1EQI2P6yWRoMz3qg@cluster0-exl40.mongodb.net/Face-Recog",  { useNewUrlParser: true,}
   );
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(cookieParser());
-app.use(morgan('dev'));
+// app.use(morgan('dev'));
 app.use(session({
     secret:'secret123',
     saveUninitialized: true,
@@ -25,13 +28,14 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
 require('./passport/passport');
-
+var us = new User();
 
 app.post('/register', function (req, res){
     console.log(req.body);
+
     const user = new User({
         username: req.body.username,
-        password: req.body.password
+        password: us.generateHash(req.body.password),
       });
 
         user
@@ -47,15 +51,34 @@ app.post('/register', function (req, res){
         });
 })
 
-app.get('/', function(req, res) {
-    console.log(req.flash);
-    res.send('Home')
+app.post('/login',
+    passport.authenticate('local', {successRedirect: '/', failureRedirect: '/login', failureFlash: true}),
+    function (req, res) {
+        res.redirect('/', req.user.username);//accessing a user object
+});
+
+app.get('/', Authenticated, function(req, res) {
+  res.sendFile(__dirname + '/pages/home.html');
   });
 
 
   app.get('/login', function(req, res) {
     res.sendFile(__dirname + '/pages/login.html');
   });
+
+  app.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/login');
+  });
+
+  function Authenticated(req, res, next){
+    if(req.isAuthenticated()){
+      return next();
+    } else {
+      //req.flash('error_msg','You are not logged in');
+      res.redirect('/login');
+    }
+  }
 
 
   app.listen(3000, function () {
--- auth_econcierge/passport/passport.js
+++ auth_econcierge/passport/passport.js
@@ -1,10 +1,20 @@
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
-var User = require('../models/user');
 
+var User = require('../models/user');
 
 var passport = require('passport')
   , LocalStrategy = require('passport-local').Strategy;
+
+passport.serializeUser(function (user, done) {
+	done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+	User.findById(id, function (err, user) {
+		done(err, user);
+	});
+})
+
+
 
 passport.use(new LocalStrategy(
   function(username, password, done) {
@@ -20,4 +30,3 @@ passport.use(new LocalStrategy(
     });
   }
 ));
-
